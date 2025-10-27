fc28c7b452848591399207e0ffbe19996f7d3285
marcos amaral <marcoscunha38@gmail.com>
2025-10-27 19:40:08 +0000

    estrutura territorios

--- war.c
+++ war.c
@@ -15,9 +15,24 @@ typedef struct Territorio{
     char cor [15];
     int tropas ;
 }territorio;
+//funções:
+int main() {
+    territorio territorios[NUM_terrotorios]={
+    {"oceania","Vermelho",10},
+    {"africa,"Azul",20},
+    {"europa","Verde",30},
+    {"america do sul ","Amarelo",40}
+    };
 
+printf("estado inicial dos territorios:\n");
+for(int i=0;i<NUM_terrotorios;i++){ 
+    printf("| cor:%s:%d tropas(%s)\n",territorios[i].cor,territorios[i].tropas,territorios[i].nome);
+}
+    char cor_atacante[10],cor_defensor[20];
+printf ("\n digite a cor do territorio atacante:");
+scanf("%s",cor_atacante);
+printf ("\n digite a cor do territorio defensor:"); 
 
-    
 
 
 
