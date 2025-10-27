45b7b423ade138a7892e1349b735cac74c0b856f
marcos amaral <marcoscunha38@gmail.com>
2025-10-27 21:40:21 +0000

    nivel mestre war struct

---
+++ nivel mestre .c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define MAX_territorios 4
+
+estrutura Territorio {
+    char nome[50];
+    int tropas;
+}territorios;
+ /iniciar territorios/
+    
+
